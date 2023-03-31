6f5f0fbd03b2d54382d9511835fe763cd8c629aa
Bernard Oduro <bigprof25@gmail.com>
2023-03-30 19:39:20 -0700

    all tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * _isupper - checks for uppercase character
- * @c: parameter
- * Return: 1 or 0
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
-int _isupper(int c);
+
+int _isupper(int c)
 {
-if ((c >= 65) && (c <= 90))
-{
-_putchar(c);
-c++;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
-_putchar('\n');
-}
+
